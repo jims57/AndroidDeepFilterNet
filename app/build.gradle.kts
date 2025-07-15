199a61f85f4351ee701765ff74b4548fb652a584
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-07-15 17:59:34 +0200

    chore: increase heap size for build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kaleyra.androiddeepfilternet"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kaleyra.androiddeepfilternet"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
